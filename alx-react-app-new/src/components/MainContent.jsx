390527922b49e3b356d31711534b936c9473a4e3
wutanglord <chemaket.chepkwony97@gmail.com>
2025-03-02 22:43:08 +0300

    styling the maincontent and footer files

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,9 +1,8 @@
 function MainContent() {
     return (
       <main style={{ padding: '20px', backgroundColor: '#f0f0f0', textAlign: 'center' }}>
-        <h2>Welcome to My Travel Blog</h2>
         <p style={{ color: 'darkgreen', fontSize: '18px' }}>
-          Explore the most beautiful cities in the world with me!
+        I love to visit New York, Paris, and Tokyo.
         </p>
       </main>
     );
